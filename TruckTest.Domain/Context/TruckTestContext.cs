c578a514f7ae3e39b99a6b56b269a0124fbbe097
Alexandre Castoldi <alexandre.castoldi@gmail.com>
2020-12-13 22:45:51 -0300

    Ensure database is created

--- TruckTest.Domain/Context/TruckTestContext.cs
+++ TruckTest.Domain/Context/TruckTestContext.cs
@@ -10,6 +10,7 @@ namespace TruckTest.Infrastructure.Context
     {
         public TruckTestContext(DbContextOptions<TruckTestContext> options) : base(options)
         {
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
